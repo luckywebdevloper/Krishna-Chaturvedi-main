df6fabd0ac269c53f8327ce7b64dc797cd3699d9
ROHAN SINGH <digitalsolutionindore@gmail.com>
2024-09-04 11:07:36 +0530

    most recent video on youtube added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 
+import { Contact_form } from "@/components/component/contact_form";
 import { Hero } from "@/components/component/hero";
+import { Video_section } from "@/components/component/video_section";
 import { Youtube_video } from "@/components/component/youtube_video";
 import Image from "next/image";
 
@@ -7,7 +9,9 @@ export default function Home() {
   return (
     <main className=" container mx-auto">
       <Hero />
-      <Youtube_video/>
+      <Youtube_video />
+      <Video_section/>
+      <Contact_form/>
   </main>
   );
 }
